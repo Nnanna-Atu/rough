c998bf408f1a42545ff2b7aff11a92da0fba008f
Buffalo <atustotle@gmail.com>
2023-12-09 11:42:02 +0100

    iter.py

--- iter.py
+++ iter.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    n = int(input())
-
-    for i in range(1, n + 1):
-        print(i, end="")
-
-    print()
+    while 1:
+        n = input('Enter length: ')
+        if n == 'end':
+            break
+        if n.isalpha():
+            print('Sorry, enter a digit.')
+        else:
+            n = int(n)
+            total = 0
+            for i in range(1, n + 1):
+                print(i, end=", ")
+                total += i
+            print(f'\nTotal: {total}')
